6f08737df8d6400a7377ff47c87f1a354bdf2110
Yosi <neugrashxsteam@hotmail.com>
2022-09-01 13:17:14 -0300

    pc

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ const db = knex({
 // console.log(db.select('*').from('users')); just an example on how to connect to the db
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
