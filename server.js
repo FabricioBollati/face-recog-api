b9efecb786ba79b0ac8142e423fc4fa70e030b14
Yosi <neugrashxsteam@hotmail.com>
2022-09-01 11:59:00 -0300

    pc

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const clarifai = require('clarifai');
 
 
 const db = knex({
@@ -24,99 +29,44 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '001',
-            name: 'jhon',
-            email: 'jhon@gmail.com',
-            password: 'jhonpw',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '002',
-            name: 'sally',
-            email: 'sally@gmail.com',
-            password: 'sallypw',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    // login: [
-    //     {
-    //         id: '004',
-    //         hash: '',
-    //         email: 'jhon@gmail.com'
-    //     }
-    // ]
-}
+// const database = {
+//     users: [
+//         {
+//             id: '001',
+//             name: 'jhon',
+//             email: 'jhon@gmail.com',
+//             password: 'jhonpw',
+//             entries: 0,
+//             joined: new Date()
+//         },
+//         {
+//             id: '002',
+//             name: 'sally',
+//             email: 'sally@gmail.com',
+//             password: 'sallypw',
+//             entries: 0,
+//             joined: new Date()
+//         }
+//     ],
+//     // login: [
+//     //     {
+//     //         id: '004',
+//     //         hash: '',
+//     //         email: 'jhon@gmail.com'
+//     //     }
+//     // ]
+// }
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('success');
 })
 
-app.post("/signin", (req, res) => {
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json('error logging in');
-    }
-})
+app.post("/signin", (req, res) => {signin.handleSignin(req, res, db, bcrypt) })
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) =>{profile.handleRegister(req, res, db) })
+app.put('/image', (req, res) => {image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res) })
 
-app.post('/register', (req, res) => {
-    const {email, password, name} =  req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-    })
-    .then( user => {
-        res.json(user)
-    })
-    .catch(err => res.status(404).json(err))
-    
-})
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-    .then( user => {
-        if (user.length){
-            res.json(user[0]);
-        } else {
-            res.status(400).json('user not found')
-        }
-    })  
-    .catch(err => res.status(400).json('error getting user'))
-
-})
-    
-
-    // if(!found){
-    //     res.status(400).json('user not found')
-    // }
-
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach( user => {
-        if(user.id === id){
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
-    })
-    if(!found){
-        res.status(400).json('user not found')
-    }
-})
-
-
-
-app.listen(3000, () => {
-    console.log('app is running on port 3000')
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app is running on port ${process.env.PORT}`)
 })
